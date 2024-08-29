d54e240901b28ef5b146dd56a1735719bda38e88
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-08-28 23:31:44 -0300

    Adicionando a Entidade User

---
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/User/model/User.java
@@ -0,0 +1,56 @@
+package br.ufpb.dcx.dsc.apiYuGiOh.User.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
