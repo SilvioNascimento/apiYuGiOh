af33c171191c79dfad2519b6cfff5bd519ad7084
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-08-29 01:13:23 -0300

    Criando uma Sub-Entidade de Card chamada CardMonster

---
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/Card/model/CardMonster.java
@@ -0,0 +1,72 @@
+package br.ufpb.dcx.dsc.apiYuGiOh.Card.model;
+
+import br.ufpb.dcx.dsc.apiYuGiOh.Card.ENUM.TipoMonster;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cache;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_card_monster")
+public class CardMonster extends Card{
+
+    @Column(name = "nivel")
+    private int nivel;
+
+    @Column(name = "atk")
+    private int atk;
+
+    @Column(name = "def")
+    private int def;
+
+    @Column(name = "atributo")
+    private String atributo;
+
+    @ElementCollection(fetch = FetchType.EAGER)  // FetchType.EAGER para carregar a lista junto com a entidade principal
+    @Enumerated(EnumType.STRING)  // Armazena o nome da enumeração como string no banco de dados
+    private List<TipoMonster> tipoMonster;
+
+    public CardMonster(){
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+
+    public List<TipoMonster> getTipoMonster() {
+        return tipoMonster;
+    }
+
+    // Fazer um controle de que apenas aceita 2 tipos, no máximo
+    public void setTipoMonster(List<TipoMonster> tipoMonster) {
+        this.tipoMonster = tipoMonster;
+    }
+}
