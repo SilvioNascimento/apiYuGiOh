9f85f1adbe8db138291a9398e1f6805747dde159
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-09-01 11:03:41 -0300

    Criando Controller para Photo

---
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/Photo/controller/PhotoController.java
@@ -0,0 +1,66 @@
+package br.ufpb.dcx.dsc.apiYuGiOh.Photo.controller;
+
+import br.ufpb.dcx.dsc.apiYuGiOh.Photo.dto.PhotoDTO;
+import br.ufpb.dcx.dsc.apiYuGiOh.Photo.model.Photo;
+import br.ufpb.dcx.dsc.apiYuGiOh.Photo.service.PhotoService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(name = "/api")
+public class PhotoController {
+    private ModelMapper modelMapper;
+    private final PhotoService photoService;
+
+    public PhotoController(ModelMapper modelMapper, PhotoService photoService) {
+        this.modelMapper = modelMapper;
+        this.photoService = photoService;
+    }
+
+    @GetMapping("/photo/{photoId}")
+    public PhotoDTO getPhoto(@PathVariable Long photoId) {
+        Photo p = photoService.getPhoto(photoId);
+        return convertToDTO(p);
+    }
+
+    @GetMapping("/photo")
+    public List<PhotoDTO> listPhotos() {
+        List<Photo> photos = photoService.listPhotos();
+        return photos.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/photo")
+    public PhotoDTO createPhoto(@RequestBody PhotoDTO photoDTO) {
+        Photo p = convertToEntity(photoDTO);
+        Photo photoCreated = photoService.savePhoto(p);
+        return convertToDTO(photoCreated);
+    }
+
+    @PutMapping("/photo/{photoId}")
+    public PhotoDTO updatePhoto(@PathVariable Long photoId,
+                                @RequestBody PhotoDTO photoDTO) {
+        Photo p = convertToEntity(photoDTO);
+        Photo photoCreated = photoService.updatePhoto(photoId, p);
+        return convertToDTO(photoCreated);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/photo/{photoId}")
+    public void deletePhoto(@PathVariable Long photoId) {
+        photoService.deletePhoto(photoId);
+    }
+
+    private PhotoDTO convertToDTO(Photo p) {
+        return modelMapper.map(p, PhotoDTO.class);
+    }
+
+    private Photo convertToEntity(PhotoDTO photoDTO) {
+        return modelMapper.map(photoDTO, Photo.class);
+    }
+}
