a3e8c731b54a5cdf93484824a324daea046b40c6
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-15 13:25:57 -0300

    Colocando a validação customizada para ver se o valor do atributo tipoSpell está válido ou não

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardSpellDTO.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardSpellDTO.java
@@ -1,10 +1,12 @@
 package br.ufpb.dcx.dsc.apiYuGiOh.dto;
 
 import br.ufpb.dcx.dsc.apiYuGiOh.ENUM.TipoSpell;
+import br.ufpb.dcx.dsc.apiYuGiOh.validation.TipoSpellExistente;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public class CardSpellDTO extends CardDTO{
     @JsonAlias({"tipospell", "tipo_spell"})
+    @TipoSpellExistente(enumClass = TipoSpell.class)
     private TipoSpell tipoSpell;
 
     public CardSpellDTO(){}
