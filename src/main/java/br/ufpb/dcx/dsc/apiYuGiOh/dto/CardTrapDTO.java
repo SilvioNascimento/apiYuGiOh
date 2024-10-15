549c64848347b2fcadcde5e18e5f0f9cdc9cf33b
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-15 07:59:36 -0300

    Adicionando novas formas de referenciar o atributo tipoTrap no JSON

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardTrapDTO.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardTrapDTO.java
@@ -1,8 +1,10 @@
 package br.ufpb.dcx.dsc.apiYuGiOh.dto;
 
 import br.ufpb.dcx.dsc.apiYuGiOh.ENUM.TipoTrap;
+import com.fasterxml.jackson.annotation.JsonAlias;
 
 public class CardTrapDTO extends CardDTO{
+    @JsonAlias({"tipotrap", "tipo_trap"})
     private TipoTrap tipoTrap;
 
     public CardTrapDTO(){}
