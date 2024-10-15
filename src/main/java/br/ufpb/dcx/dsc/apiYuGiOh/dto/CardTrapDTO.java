eb8bc928480db9a9dd958b2166aa32466b366414
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-15 13:26:10 -0300

    Colocando a validação customizada para ver se o valor do atributo tipoTrap está válido ou não

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardTrapDTO.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/dto/CardTrapDTO.java
@@ -1,10 +1,12 @@
 package br.ufpb.dcx.dsc.apiYuGiOh.dto;
 
 import br.ufpb.dcx.dsc.apiYuGiOh.ENUM.TipoTrap;
+import br.ufpb.dcx.dsc.apiYuGiOh.validation.TipoTrapExistente;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public class CardTrapDTO extends CardDTO{
     @JsonAlias({"tipotrap", "tipo_trap"})
+    @TipoTrapExistente(enumClass = TipoTrap.class)
     private TipoTrap tipoTrap;
 
     public CardTrapDTO(){}
