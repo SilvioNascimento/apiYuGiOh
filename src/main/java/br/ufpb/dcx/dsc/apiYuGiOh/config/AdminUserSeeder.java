1811cda8f052823258f37058712f254b305d475f
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-21 11:13:39 -0300

    Adicionando uma configuração para criar um User ADMIN

---
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/config/AdminUserSeeder.java
@@ -0,0 +1,34 @@
+package br.ufpb.dcx.dsc.apiYuGiOh.config;
+
+import br.ufpb.dcx.dsc.apiYuGiOh.ENUM.Role;
+import br.ufpb.dcx.dsc.apiYuGiOh.model.User;
+import br.ufpb.dcx.dsc.apiYuGiOh.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class AdminUserSeeder {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminUserSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void seedAdminUser() {
+        if (userRepository.findByUsername("admin") == null) {
+            User admin = new User();
+            admin.setNome("Admin");
+            admin.setEmail("admin@gmail.com");
+            admin.setUsername("admin");
+            admin.setSenha(passwordEncoder.encode("admin123"));
+            admin.setRoles(Set.of(Role.ADMIN));
+            userRepository.save(admin);
+        }
+    }
+}
