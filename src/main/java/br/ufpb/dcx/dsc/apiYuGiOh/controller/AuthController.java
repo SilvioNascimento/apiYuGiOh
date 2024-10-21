62c6e9ccd5b5768a7704a8338c65c96f00bfe0a5
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-21 11:13:09 -0300

    Adicionando o AuthController

---
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/controller/AuthController.java
@@ -0,0 +1,89 @@
+package br.ufpb.dcx.dsc.apiYuGiOh.controller;
+
+import br.ufpb.dcx.dsc.apiYuGiOh.ENUM.Role;
+import br.ufpb.dcx.dsc.apiYuGiOh.dto.*;
+import br.ufpb.dcx.dsc.apiYuGiOh.exception.UserNotFoundException;
+import br.ufpb.dcx.dsc.apiYuGiOh.model.User;
+import br.ufpb.dcx.dsc.apiYuGiOh.repository.UserRepository;
+import br.ufpb.dcx.dsc.apiYuGiOh.security.JwtUtil;
+import br.ufpb.dcx.dsc.apiYuGiOh.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.modelmapper.ModelMapper;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping(path = "/api")
+@Validated
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AuthController( UserService userService, ModelMapper modelMapper, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @PostMapping("/login")
+    public TokenResponseDTO createAuthenticationToken(@RequestBody LoginRequestDTO loginRequestDTO) throws Exception {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequestDTO.getUsername(), loginRequestDTO.getSenha())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequestDTO.getUsername());
+        String token = jwtUtil.generateToken(userDetails);
+        return new TokenResponseDTO(token);
+
+    }
+
+    @PostMapping("/change-password")
+    public String changePassword(@RequestBody ChangePasswordRequestDTO changePasswordRequestDTO) {
+        User user = userService.getUserByUsername(changePasswordRequestDTO.getUsername());
+        if(user != null) {
+            userService.updateUser(user.getId(), user);
+            return "Senha alterada com sucesso!";
+        }
+        throw new UserNotFoundException("User " + user.getUsername() + " não foi encontrado!");
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTOResponse registerUser(@Valid @RequestBody RegisterRequestDTO registerRequestDTO) {
+        User user = new User();
+        user.setNome(registerRequestDTO.getNome());
+        user.setEmail(registerRequestDTO.getEmail());
+        user.setUsername(registerRequestDTO.getUsername());
+        user.setSenha(bCryptPasswordEncoder.encode(registerRequestDTO.getSenha()));
+        user.setRoles(Set.of(Role.USER));
+        System.out.println(user.getRoles());
+
+        User saved = userService.createUser(user);
+        return convertToDTO(saved);
+
+    }
+
+    private UserDTOResponse convertToDTO(User u) {
+        return modelMapper.map(u, UserDTOResponse.class);
+    }
+}
