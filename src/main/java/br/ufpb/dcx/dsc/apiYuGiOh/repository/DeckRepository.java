2621135930bef865110ca4482415bd3ccaba80c7
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-21 11:07:47 -0300

    Adicionando mais uma assinatura para o repositório

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/repository/DeckRepository.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/repository/DeckRepository.java
@@ -4,5 +4,5 @@ import br.ufpb.dcx.dsc.apiYuGiOh.model.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeckRepository extends JpaRepository<Deck, Long> {
-
+    Deck findByNomeDeck(String nomeDeck);
 }
