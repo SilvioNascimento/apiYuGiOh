734c9f6d565bb7588e3bc9c85d5f1e14365dd315
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-21 04:44:20 -0300

    Adicionando a assinatura do método que procura um User pelo username

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/repository/UserRepository.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/repository/UserRepository.java
@@ -4,4 +4,5 @@ import br.ufpb.dcx.dsc.apiYuGiOh.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
