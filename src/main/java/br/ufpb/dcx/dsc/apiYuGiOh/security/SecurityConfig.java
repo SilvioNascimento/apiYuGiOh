daae9a87b344ea542ac46c77b2cf19d97d9d8392
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-10-21 21:45:17 -0300

    Atribuindo os papéis para os novos endpoints

--- src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/security/SecurityConfig.java
+++ src/main/java/br/ufpb/dcx/dsc/apiYuGiOh/security/SecurityConfig.java
@@ -34,6 +34,9 @@ public class SecurityConfig{
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/login", "/api/change-password", "/api/register", "/h2-ui/**").permitAll()
 
                         //EndPoints de User
+                        //Permitir tanto ADMIN quanto USER possam acessar as rotas PUT's específicas
+                        .requestMatchers(HttpMethod.PUT, "/user/{userId}/deck/{deckId}/add").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/user/{userId}/deck/{deckId}/remove").hasAnyRole("USER", "ADMIN")
                         //Permitir apenas ADMIN acessar as rotas POST, PUT, DELETE e GET
                         .requestMatchers(HttpMethod.GET, "/api/user/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/user/**").hasRole("ADMIN")
